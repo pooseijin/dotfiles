82c781173403efae272f50acd6863e482613883d
pooseijin <pooseijin@motohidentaku.com>
2019-05-04 11:13:51 +0900

    vim

---
+++ etc/install.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -e
+set -u
+
+install() {
+  dotfiles=$HOME/dotfiles
+
+  symlink() {
+    [ -e "$2" ] || ln -s "$1" "$2"
+  }
+
+  if [ -d "$dotfiles" ]; then
+    (cd "$dotfiles" && git pull --rebase)
+  else
+    git clone https://github.com/pooseijin/dotfiles "$dotfiles"
+  fi
+
+  symlink "$dotfiles/.vimrc" "$HOME/.vimrc"
+}
+
+install
